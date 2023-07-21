457a089f022a33fab24915c11047d81327e441d2
Ricardo Bautista <rickyhaswifi@gmail.com>
2023-07-21 07:10:25 -0500

    fix build 2

--- src/pages/static/[pageSlug].tsx
+++ src/pages/static/[pageSlug].tsx
@@ -1,41 +1,42 @@
-import {
-  GetStaticProps,
-  GetStaticPaths,
-} from "next";
-import { sdkClient } from "@/lib/graphql-client";
-import type { LayoutQuery } from "@/graphql/generated/graphql";
+// import {
+//   GetStaticProps,
+//   GetStaticPaths,
+// } from "next";
+// import { sdkClient } from "@/lib/graphql-client";
+// import type { LayoutQuery } from "@/graphql/generated/graphql";
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  const pagesSlugList = await sdkClient.pagesSlugList();
-  return {
-    paths: pagesSlugList.pages?.map(item => {
-      return {
-        params: {
-          pageSlug: item.pageSlug as string
-        }
-      }
-    }) ?? [],
-    fallback: true, // false or "blocking"
-  };
-};
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   const pagesSlugList = await sdkClient.pagesSlugList();
+//   return {
+//     paths: pagesSlugList.pages?.map(item => {
+//       return {
+//         params: {
+//           pageSlug: item.pageSlug as string
+//         }
+//       }
+//     }) ?? [],
+//     fallback: true, // false or "blocking"
+//   };
+// };
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const layout: LayoutQuery = await sdkClient.layout({
-    pageSlug: params?.pageSlug as string,
-  });
-  console.log("getStaticProps", layout.page?.pageSlug);
-  return {
-    props: {
-      layout,
-    },
-  };
-};
+// export const getStaticProps: GetStaticProps = async ({ params }) => {
+//   const layout: LayoutQuery = await sdkClient.layout({
+//     pageSlug: params?.pageSlug as string,
+//   });
+//   console.log("getStaticProps", layout.page?.pageSlug);
+//   return {
+//     props: {
+//       layout,
+//     },
+//   };
+// };
 
 
-export default function Page({ layout }: { layout: LayoutQuery }) {
+// export default function Page({ layout }: { layout: LayoutQuery }) {
+export default function PageStatic() {
   return (
     <>
-      <h1>{layout.page?.id}</h1>
+      {/* <h1>{layout.page?.id}</h1> */}
       {/* <Home layout={layout} />     */}
     </>
   );
