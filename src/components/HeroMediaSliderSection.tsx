a52164cc2453c3b8df5506e50518ad5470ca86a6
Ricardo Bautista <rickyhaswifi@gmail.com>
2023-08-02 00:40:38 -0500

    fix mobile issue

--- src/components/HeroMediaSliderSection.tsx
+++ src/components/HeroMediaSliderSection.tsx
@@ -62,7 +62,7 @@ export default function HeroMediaSliderSection({
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    arrows: true,
+    arrows: false,
     draggable: true,
     pauseOnHover: true,
   };
@@ -73,144 +73,138 @@ export default function HeroMediaSliderSection({
           <Slider {...settings} className="">
             {heroMediaSlider.map((heroMediaSliderItem, index) => (
               <section
-                key={heroMediaSliderItem.id}
+                key={`${heroMediaSliderItem.id}-${index++}`}
                 className={`video-wrapper dark-section ${
                   heroMediaSliderItem?.sliderCssWrapper &&
                   heroMediaSliderItem.sliderCssWrapper
                 }`}
               >
                 <div className="video-overlay-content headlinePrimary">
-                  <div key={Math.random()} className="">
-                    <div
-                      className={`p-4 text-${
-                        heroMediaSliderItem?.textContent?.contentAlign &&
-                        heroMediaSliderItem?.textContent?.contentAlign?.toLocaleLowerCase()
-                      }`}
-                    >
-                      <Fade direction="up" triggerOnce>
-                        {heroMediaSliderItem?.textContent?.contentImage && (
-                          <Image
-                            width={0}
-                            height={0}
-                            sizes="100%"
-                            className={`block mb-0 ${
-                              (heroMediaSliderItem?.textContent
-                                ?.contentAlign === "center" &&
-                                "mx-auto") ||
-                              (heroMediaSliderItem?.textContent
-                                ?.contentAlign === "left" &&
-                                "mr-auto") ||
-                              (heroMediaSliderItem?.textContent
-                                ?.contentAlign === "right" &&
-                                "ml-auto") ||
-                              (heroMediaSliderItem?.textContent
-                                ?.contentAlign === "justify" &&
-                                "mx-auto")
-                            } ${heroMediaSliderItem?.textContent?.imageStyle?.map(
-                              (imageStyleItem) =>
-                                ` dynamic-image-class dynamic-${imageStyleItem} `
-                            )}`}
-                            style={{ objectFit: "contain" }}
-                            src={
-                              heroMediaSliderItem?.textContent?.contentImage.url
-                            }
-                            alt={
-                              heroMediaSliderItem?.textContent?.header?.html ||
-                              ""
-                            }
-                          />
-                        )}
-                      </Fade>
-                      <div className="animate-fade-in-up">
-                        {heroMediaSliderItem?.textContent?.header && (
-                          <div className="text-shadow body-parsed-text">
-                            {parse(
-                              heroMediaSliderItem?.textContent.header.html
-                            )}
-                          </div>
-                        )}
-                      </div>
-                      <div className="animate-fade-in-up">
-                        {heroMediaSliderItem?.textContent?.subHeader && (
-                          <div className="body-parsed-text">
-                            {parse(
-                              heroMediaSliderItem?.textContent.subHeader.html
-                            )}
-                          </div>
-                        )}
-                      </div>
-                      <div className="animate-fade-in-up">
-                        {heroMediaSliderItem?.textContent?.content && (
-                          <div className="body-parsed-text">
-                            {parse(
-                              heroMediaSliderItem?.textContent.content.html
-                            )}
-                          </div>
-                        )}
-                      </div>
-                      <div className="animate-fade-in-up">
-                        <div className="flex flex-row flex-wrap justify-center items-center mx-auto ">
-                          {heroMediaSliderItem?.callToAction?.map(
-                            (callToActionItem) => (
-                              <div key={Math.random()}>
-                                {callToActionItem?.ctaLink && (
-                                  <div
-                                    className={`flex ${
-                                      (callToActionItem?.contentAlign ===
-                                        "center" &&
-                                        "mx-auto") ||
-                                      (callToActionItem?.contentAlign ===
-                                        "left" &&
-                                        "mr-auto") ||
-                                      (callToActionItem?.contentAlign ===
-                                        "right" &&
-                                        "ml-auto") ||
-                                      (callToActionItem?.contentAlign ===
-                                        "justify" &&
-                                        "mx-auto")
-                                    }`}
-                                  >
-                                    {callToActionItem?.ctaLink.includes(
-                                      "http"
-                                    ) ? (
-                                      <a
-                                        href={callToActionItem?.ctaLink}
-                                        target="_blank"
-                                        className={`${
-                                          callToActionItem.ctaClass
-                                        } ${
-                                          callToActionItem?.ctaPrimary
-                                            ? "border-white text-white border px-4 md:px-6 py-2 theme-button max-w-max block no-underline my-4 font-bold w-full text-2xl"
-                                            : "text-white border-0 px-4 md:px-6 py-2 theme-button max-w-max block no-underline my-4 w-full text-2xl"
-                                        } mr-2 max-w-max`}
-                                        rel="noreferrer"
-                                      >
-                                        {callToActionItem.ctaLabel}
-                                      </a>
-                                    ) : (
-                                      <Link
-                                        href={callToActionItem?.ctaLink}
-                                        className={`${
-                                          callToActionItem.ctaClass
-                                        } ${
-                                          callToActionItem?.ctaPrimary
-                                            ? "border-white text-white border px-4 md:px-6 py-2 theme-button max-w-max block no-underline my-4 font-bold w-full text-2xl"
-                                            : "text-white border-0 px-4 md:px-6 py-2 theme-button max-w-max block no-underline my-4 w-full text-2xl"
-                                        } mr-2 max-w-max`}
-                                      >
-                                        {callToActionItem.ctaLabel}
-                                      </Link>
-                                    )}
-                                  </div>
-                                )}
-                              </div>
-                            )
+                  <div
+                    className={`p-4 text-${
+                      heroMediaSliderItem?.textContent?.contentAlign &&
+                      heroMediaSliderItem?.textContent?.contentAlign?.toLocaleLowerCase()
+                    }`}
+                  >
+                    <Fade direction="up" triggerOnce>
+                      {heroMediaSliderItem?.textContent?.contentImage && (
+                        <Image
+                          width={0}
+                          height={0}
+                          sizes="100%"
+                          className={`block mb-0 ${
+                            (heroMediaSliderItem?.textContent?.contentAlign ===
+                              "center" &&
+                              "mx-auto") ||
+                            (heroMediaSliderItem?.textContent?.contentAlign ===
+                              "left" &&
+                              "mr-auto") ||
+                            (heroMediaSliderItem?.textContent?.contentAlign ===
+                              "right" &&
+                              "ml-auto") ||
+                            (heroMediaSliderItem?.textContent?.contentAlign ===
+                              "justify" &&
+                              "mx-auto")
+                          } ${heroMediaSliderItem?.textContent?.imageStyle?.map(
+                            (imageStyleItem) =>
+                              ` dynamic-image-class dynamic-${imageStyleItem} `
+                          )}`}
+                          style={{ objectFit: "contain" }}
+                          src={
+                            heroMediaSliderItem?.textContent?.contentImage.url
+                          }
+                          alt={
+                            heroMediaSliderItem?.textContent?.header?.html || ""
+                          }
+                        />
+                      )}
+                    </Fade>
+                    <div className="animate-fade-in-up">
+                      {heroMediaSliderItem?.textContent?.header && (
+                        <div className="text-shadow body-parsed-text">
+                          {parse(heroMediaSliderItem?.textContent.header.html)}
+                        </div>
+                      )}
+                    </div>
+                    <div className="animate-fade-in-up">
+                      {heroMediaSliderItem?.textContent?.subHeader && (
+                        <div className="body-parsed-text">
+                          {parse(
+                            heroMediaSliderItem?.textContent.subHeader.html
                           )}
                         </div>
+                      )}
+                    </div>
+                    <div className="animate-fade-in-up">
+                      {heroMediaSliderItem?.textContent?.content && (
+                        <div className="body-parsed-text">
+                          {parse(heroMediaSliderItem?.textContent.content.html)}
+                        </div>
+                      )}
+                    </div>
+                    <div className="animate-fade-in-up">
+                      <div className="flex flex-row flex-wrap justify-center items-center mx-auto ">
+                        {heroMediaSliderItem?.callToAction?.map(
+                          (callToActionItem) => (
+                            <div key={Math.random()}>
+                              {callToActionItem?.ctaLink && (
+                                <div
+                                  className={`flex ${
+                                    (callToActionItem?.contentAlign ===
+                                      "center" &&
+                                      "mx-auto") ||
+                                    (callToActionItem?.contentAlign ===
+                                      "left" &&
+                                      "mr-auto") ||
+                                    (callToActionItem?.contentAlign ===
+                                      "right" &&
+                                      "ml-auto") ||
+                                    (callToActionItem?.contentAlign ===
+                                      "justify" &&
+                                      "mx-auto")
+                                  }`}
+                                >
+                                  {callToActionItem?.ctaLink.includes(
+                                    "http"
+                                  ) ? (
+                                    <a
+                                      href={callToActionItem?.ctaLink}
+                                      target="_blank"
+                                      className={`${
+                                        callToActionItem.ctaClass
+                                      } ${
+                                        callToActionItem?.ctaPrimary
+                                          ? "border-white text-white border px-4 md:px-6 py-2 theme-button max-w-max block no-underline my-4 font-bold w-full text-2xl"
+                                          : "text-white border-0 px-4 md:px-6 py-2 theme-button max-w-max block no-underline my-4 w-full text-2xl"
+                                      } mr-2 max-w-max`}
+                                      rel="noreferrer"
+                                    >
+                                      {callToActionItem.ctaLabel}
+                                    </a>
+                                  ) : (
+                                    <Link
+                                      href={callToActionItem?.ctaLink}
+                                      className={`${
+                                        callToActionItem.ctaClass
+                                      } ${
+                                        callToActionItem?.ctaPrimary
+                                          ? "border-white text-white border px-4 md:px-6 py-2 theme-button max-w-max block no-underline my-4 font-bold w-full text-2xl"
+                                          : "text-white border-0 px-4 md:px-6 py-2 theme-button max-w-max block no-underline my-4 w-full text-2xl"
+                                      } mr-2 max-w-max`}
+                                    >
+                                      {callToActionItem.ctaLabel}
+                                    </Link>
+                                  )}
+                                </div>
+                              )}
+                            </div>
+                          )
+                        )}
                       </div>
                     </div>
                   </div>
                 </div>
+
                 {!!heroMediaSliderItem?.displaySocialMedia && (
                   <div className="tagline-wrapper">
                     <SocialMediaIcons siteLibrary={siteLibrary} />
