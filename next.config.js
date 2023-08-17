e483faea31f4445e7c6036b154aad50c6af5af05
joshuadutton <joshuadutton@gmail.com>
2023-08-17 16:27:09 -0500

    Try next rewrite rule

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   images: {
     domains: ["media.graphcms.com", "media.graphassets.com"],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/sitemap.xml',
+        destination: '/sitemap-custom.xml',
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
