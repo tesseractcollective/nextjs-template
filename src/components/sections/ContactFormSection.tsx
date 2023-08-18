d6c2b106302beaced526d773a33b210c2fb7e757
Ricardo Bautista <rickyhaswifi@gmail.com>
2023-08-18 12:56:18 -0500

    update IDs

--- src/components/sections/ContactFormSection.tsx
+++ src/components/sections/ContactFormSection.tsx
@@ -32,7 +32,7 @@ export default function ContactFormSection({
   } = useForm({ mode: "onBlur" });
 
   const netlify = useNetlifyForm({
-    name: "contact",
+    name: "contactCustom",
     action: "#",
     honeypotName: "bot-field",
     onSuccess: (response: any, context: any) => {
@@ -149,7 +149,6 @@ export default function ContactFormSection({
                               })}
                               id="name"
                               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                              placeholder="you@gmail.com"
                             />
                           </div>
                         </div>
@@ -169,7 +168,6 @@ export default function ContactFormSection({
                                 required: "phone is required",
                               })}
                               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                              placeholder="you@gmail.com"
                             />
                           </div>
                         </div>
