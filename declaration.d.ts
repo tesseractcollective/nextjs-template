400eff262dc79f3eea43b833b7419acbc9a9bb10
Ricardo Bautista <rickyhaswifi@gmail.com>
2023-08-18 10:59:18 -0500

    neftlify test

--- declaration.d.ts
+++ declaration.d.ts
@@ -1,4 +1,5 @@
-declare module 'react-hubspot-form';
-declare module 'react-jotform-embed';
-declare module 'react-slick';
-declare module 'react-magic-grid';
+declare module "react-hubspot-form";
+declare module "react-jotform-embed";
+declare module "react-slick";
+declare module "react-magic-grid";
+declare module "react-netlify-forms";
--- src/components/sections/ContactFormSection.tsx
+++ src/components/sections/ContactFormSection.tsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import parse from "html-react-parser";
 import HubspotForm from "react-hubspot-form";
 import JotformEmbed from "react-jotform-embed";
-import ReactGA from "react-ga4";
 import type {
   SiteLibraryFieldsFragment,
   ContactFormFieldsFragment,
@@ -23,6 +22,14 @@ interface ContactFormProps {
   siteLibrary: SiteLibraryFieldsFragment;
 }
 
+export interface NetlifyFormState {
+  loading: boolean;
+  error: boolean;
+  success: boolean;
+  recaptchaError?: boolean | undefined;
+  recaptcha?: React.ReactElement | undefined;
+}
+
 export default function ContactFormSection({
   contactFormData,
   siteLibrary,
@@ -35,7 +42,6 @@ export default function ContactFormSection({
   } = useForm<FormPost>();
   if (!siteLibrary) return <></>;
   const { isSpanish } = siteLibrary;
-
   const encode = (data: any) => {
     return Object.keys(data)
       .map(
@@ -49,9 +55,10 @@ export default function ContactFormSection({
     /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
   const onSubmit: SubmitHandler<FormPost> = (data) => {
-    fetch("/contact", {
+    fetch("/", {
+      method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ contact: "contact", ...data }),
+      body: encode({ "form-name": "contact", ...data }),
     })
       .then(() => console.log("Success!"))
       .catch((error) => console.log(error));
@@ -84,7 +91,13 @@ export default function ContactFormSection({
                 </div>
               )}
               {submitted && (
-                <p style={{ color: "green" }}>Successfully submitted form!</p>
+                <div className="bg-[#38fa8c] text-center border-[#229a2a] rounded-sm border p-4">
+                  <p className="text-[#229a2a]">
+                    {isSpanish
+                      ? "¡Formulario enviado con éxito!"
+                      : "Successfully submitted form!"}
+                  </p>
+                </div>
               )}
               {!submitted &&
                 !!contactFormItem?.netlifyContactForm &&
@@ -94,109 +107,47 @@ export default function ContactFormSection({
                     method="POST"
                     data-netlify="true"
                     onSubmit={handleSubmit(onSubmit)}
-                    action="#?formSubmit=success"
                   >
-                    <input type="hidden" name="contact" value="contact" />
-                    <section className="container flex-col grid mx-auto dark-section">
-                      <div className="w-full mx-auto">
-                        <div className="form-input-wrapper border-round p-2">
-                          <div className="mb-2">
-                            <label
-                              htmlFor="email"
-                              className="block text-sm font-medium leading-6 text-dark"
-                            >
-                              Email
-                            </label>
-                            <div className="relative mt-2 rounded-md shadow-sm">
-                              <input
-                                type="email"
-                                id="email"
-                                placeholder="Email"
-                                {...register("email")}
-                                className="block w-full rounded-md border-0 py-1.5 pr-10 text-red-900 ring-1 ring-inset ring-red-300 placeholder:text-red-300 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6"
-                              />
-                              {errors.email?.type === "required" ? (
-                                <p>Email is required</p>
-                              ) : errors.email?.type === "pattern" ? (
-                                <p>Invalid email</p>
-                              ) : (
-                                <></>
-                              )}
-                            </div>
-                          </div>
-
-                          <div className="mb-2">
-                            <label
-                              htmlFor="Name"
-                              className="block text-sm font-medium leading-6 text-dark"
-                            >
-                              {isSpanish ? "Nombre" : "Name"}
-                            </label>
-                            <div className="mt-2">
-                              <input
-                                type="text"
-                                placeholder="name"
-                                {...register("name")}
-                                id="name"
-                                className="block w-full rounded-md border-0 py-1.5 text-dark shadow-sm ring-1 ring-inset ring-dark placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
-                              />
-                            </div>
-                          </div>
-
-                          <div className="mb-2">
-                            <label
-                              htmlFor="phone"
-                              className="block text-sm font-medium leading-6 text-dark"
-                            >
-                              {isSpanish ? "Tel" : "Phone"}
-                            </label>
-                            <div className="mt-2">
-                              <input
-                                type="text"
-                                id="phone"
-                                {...register("phone")}
-                                className="block w-full rounded-md border-0 py-1.5 text-dark shadow-sm ring-1 ring-inset ring-dark placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
-                                placeholder="281-330-8004"
-                              />
-                            </div>
-                          </div>
-
-                          <div>
-                            <label
-                              htmlFor="Message"
-                              className="block text-sm font-medium leading-6 text-dark"
-                            >
-                              {isSpanish ? "Mensaje" : "Message"}
-                            </label>
-                            <div className="mt-2">
-                              <textarea
-                                rows={4}
-                                placeholder="message"
-                                {...register("message")}
-                                name="message"
-                                id="message"
-                                className="block w-full rounded-md border-0 py-1.5 text-dark shadow-sm ring-1 ring-inset ring-dark placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
-                              />
-                            </div>
-                          </div>
-                          <div>
-                            <button
-                              type="submit"
-                              onClick={() => {
-                                ReactGA.event({
-                                  category: "Interaction",
-                                  action: `Contact Page - Form Submit`,
-                                  label: `Contact Page - Form Submit`,
-                                });
-                              }}
-                              className="flex w-full justify-center rounded-md bg-primary px-3 py-1.5 text-sm font-semibold leading-6 !text-white shadow-sm hover:bg-primary-hover transition focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 my-2"
-                            >
-                              {isSpanish ? "Enviar" : "Submit"}
-                            </button>
-                          </div>
-                        </div>
+                    <input type="hidden" name="form-name" value="contact" />
+                    <div>
+                      <div>
+                        <input
+                          type="text"
+                          id="name"
+                          placeholder="First Name"
+                          {...register("name")}
+                        />
                       </div>
-                    </section>
+                      <div>
+                        <input
+                          type="text"
+                          id="message"
+                          placeholder="Message"
+                          {...register("message")}
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <input
+                        type="text"
+                        id="email"
+                        placeholder="Email"
+                        {...register("email", {
+                          required: true,
+                          pattern: emailRegex,
+                        })}
+                      />
+                      {errors.email?.type === "required" ? (
+                        <p>Email is required</p>
+                      ) : errors.email?.type === "pattern" ? (
+                        <p>Invalid email</p>
+                      ) : (
+                        <></>
+                      )}
+                    </div>
+                    <div>
+                      <button type="submit">Get Access</button>
+                    </div>
                   </form>
                 )}
 
