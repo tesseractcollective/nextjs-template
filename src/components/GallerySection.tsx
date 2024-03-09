c38c217df3382208468c6a7e82ba3e059d4443e2
Ricardo Bautista <rickyhaswifi@gmail.com>
2024-03-08 21:22:50 -0600

    fix ui

--- src/components/GallerySection.tsx
+++ src/components/GallerySection.tsx
@@ -42,6 +42,27 @@ export default function GallerySection({
   const finalImages = gallery.map((image) => image.url);
   if (!finalImages) return <></>;
 
+  if (galleryLayout === "fullScreen" && gallery.length >= 1) {
+    return (
+      <div className="relative">
+        {finalImages.map((finalImage, index) => (
+          <section key={finalImage} className="h-100vh bg-bg top-0 sticky">
+            <div className="absolute w-full h-full top-0 left-0 bg-cover bg-center bg-no-repeat duration-[400ms] bg-fixed"></div>
+            {finalImage && (
+              <Image
+                src={finalImage}
+                alt=""
+                width={0}
+                height={0}
+                sizes="100%"
+                className="w-full h-full object-cover absolute inset-0 z-0 h-100vh"
+              />
+            )}
+          </section>
+        ))}
+      </div>
+    );
+  }
   if (galleryLayout === "grid" && gallery.length >= 1) {
     return (
       <div className="my-0 magic-grid block h-full">
--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -543,14 +543,14 @@ export default function PageHeader({
         </Zoom>
       )}
       {pageWidthStyle === "moon" && (
-        <Fade triggerOnce>
+        <Fade triggerOnce className="relative">
           <div
             className={`relative block transition-all duration-500 ${
               scroll ? "" : "bg-primary"
             }`}
           >
-            <div className="flex flex-col items-center justify-center min-h-[65vh]">
-              <div className="py-2 mx-auto relative w-full text-color">
+            <div className="flex flex-col items-center justify-center min-h-[65vh] relative z-40">
+              <div className="py-2 mx-auto relative w-full text-color items-center flex justify-center flex-col">
                 <Fade direction="up" triggerOnce>
                   {!!pageHeaderTitleProp && (
                     <h1 className="text-3xl md:text-6xl xl:text-7xl  mt-0 mb-1 py-0 text-center font-bold uppercase">
@@ -563,42 +563,66 @@ export default function PageHeader({
                     </h2>
                   )}
                   {!!pageCallToAction && pageCallToAction?.length > 0 && (
-                    <div>
-                      <div className="text-center mx-auto">
-                        {pageCallToAction.map(
-                          (callToActionItem) =>
-                            callToActionItem?.ctaLink && (
-                              <div
-                                key={callToActionItem.ctaLink}
-                                className="text-center mx-auto"
-                              >
-                                <LinkItem
-                                  link={callToActionItem.ctaLink}
-                                  label={callToActionItem.ctaLabel}
-                                  cssClass={
-                                    callToActionItem?.ctaClass
-                                      ? callToActionItem.ctaClass
-                                      : `${
-                                          callToActionItem.ctaPrimary
-                                            ? "border-white text-text-color border px-4 md:px-6 py-2 theme-button max-w-max block no-underline my-4 font-bold w-full text-2xl"
-                                            : "text-text-color border-0 px-4 md:px-6 py-2 theme-button max-w-max block no-underline my-4 w-full text-2xl"
-                                        } mr-2 max-w-max`
-                                  }
-                                />
-                              </div>
-                            )
-                        )}
-                      </div>
+                    <div className="flex items-center flex-row justify-center gap-x-4 text-center mx-auto">
+                      {pageCallToAction.map(
+                        (callToActionItem) =>
+                          callToActionItem?.ctaLink && (
+                            <div
+                              key={callToActionItem.ctaLink}
+                              className="text-center mx-auto"
+                            >
+                              <LinkItem
+                                link={callToActionItem.ctaLink}
+                                label={callToActionItem.ctaLabel}
+                                cssClass={
+                                  callToActionItem?.ctaClass
+                                    ? callToActionItem.ctaClass
+                                    : `${
+                                        callToActionItem.ctaPrimary
+                                          ? "border-white text-text-color border px-4 md:px-6 py-2 theme-button max-w-max block no-underline my-4 font-bold w-full text-2xl bg-secondary !rounded-md"
+                                          : "text-text-color border-0 px-4 md:px-6 py-2 theme-button max-w-max block no-underline my-4 w-full text-2xl !border !rounded-md"
+                                      } mr-2 max-w-max`
+                                }
+                              />
+                            </div>
+                          )
+                      )}
                     </div>
                   )}
                 </Fade>
               </div>
             </div>
+            <div
+              className="absolute inset-x-0 -top-40 z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+              aria-hidden="true"
+            >
+              <div
+                className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-secondary to-tertiary opacity-50 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+                style={{
+                  clipPath:
+                    "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+                }}
+              />
+            </div>
+            <Zoom triggerOnce>
+              <div
+                className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+                aria-hidden="true"
+              >
+                <div
+                  className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr  from-secondary to-tertiary opacity-50  sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+                  style={{
+                    clipPath:
+                      "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+                  }}
+                />
+              </div>
+            </Zoom>
             {pageHeaderImageProp && (
               <div
-                className={`relative w-full mx-auto block h-full transition-all overflow-hidden rounded-full aspect-1 ${
+                className={`relative w-full mx-auto block h-full transition-all overflow-hidden rounded-full aspect-1 duration-700 ${
                   scroll
-                    ? "max-w-[1880px] max-h-[1880px] rotate-10"
+                    ? "max-w-[1880px] max-h-[1880px] rotate-180"
                     : "max-w-[1440px] max-h-[1440px] rotate-0"
                 }`}
               >
