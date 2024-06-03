a573418f0c69b66af7b1b656ff16d07329b30979
Ricardo Bautista <rickyhaswifi@gmail.com>
2024-06-03 13:39:43 -0500

    fix ProfileMusicTwoPage build

--- src/components/ProfilePageLayouts/ProfileMusicTwoPage.tsx
+++ src/components/ProfilePageLayouts/ProfileMusicTwoPage.tsx
@@ -449,7 +449,7 @@ export default function ProfileMusicTwoPage({
           profileType={profile.profileType}
           siteID={siteLibrary.siteId}
           siteLogo={siteLibrary?.logo?.url}
-          siteDomain={siteLibrary.metaDomain}
+          siteDomain={siteLibrary.metaDomain || ""}
         />
       )}
     </div>
