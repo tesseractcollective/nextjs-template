e35be64d59f62b4f5014f40d4d6a9743bbe11e59
Ricardo Bautista <rickyhaswifi@gmail.com>
2024-10-26 00:09:57 -0500

    fix page header

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -962,6 +962,39 @@ export default function PageHeader({
           )}
         </section>
       )}
+      {pageWidthStyle === "square" && (
+        <div className="w-full p-2">
+          {/* 16:9 aspect ratio container */}
+          <div className="relative w-full h-0 pb-[56.25%]">
+            {/* Content container */}
+            <div className="absolute inset-0 header-overlay topper">
+              {!!pageHeaderImageProp && (
+                <Image
+                  src={pageHeaderImageProp}
+                  alt=""
+                  width={0}
+                  height={0}
+                  sizes="100%"
+                  className="w-full h-full object-cover rounded-4xl"
+                />
+              )}
+
+              {/* Dark overlay */}
+              <div className="absolute bg-gradient-to-t from-[#000] rounded-4xl z-20 h-32 opacity-70 left-0 right-0 bottom-0" />
+
+              {/* Content */}
+              <div className="absolute z-30 inset-0 p-4 md:p-8 lg:p-12 flex flex-col justify-end items-center">
+                <h3 className="text-2xl md:text-3xl lg:text-4xl font-bold text-text-overlay mb-2 md:mb-4 text-shadow text-center">
+                  {pageHeaderTitleProp}
+                </h3>
+                <p className="text-lg text-text-overlay max-w-prose text-shadow text-center">
+                  {pageHeaderSubtitleProp}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
       {pageWidthStyle === "Content" && (
         <section
           className={`relative mx-auto max-w-8xl px-4 sm:px-6 lg:px-8' ${cssClass}`}
